aefa402fca5aecafcbb0ca6a820355d2c476182c
Ivanilso da Silva Conceicão Filho <ivanilsodasilva300@gmail.com>
2024-02-28 00:56:01 -0300

    FEAT: Atualização das informações da conexao

--- src/main/singleton/database/DatabaseConnection.kt
+++ src/main/singleton/database/DatabaseConnection.kt
@@ -11,9 +11,9 @@ class DatabaseConnection {
     public fun getConnection(): Connection {
         if (connection == null) {
             // Configurações de conexão com o banco de dados
-            val url = "jdbc:mysql://localhost:3306/test"
-            val username = "root"
-            val password = ""
+            val url = "jdbc:mysql://localhost:3306/banco_de_dados"
+            val username = "usuario"
+            val password = "senha"
 
             // Estabelece a conexão com o banco de dados
             connection = DriverManager.getConnection(url, username, password)

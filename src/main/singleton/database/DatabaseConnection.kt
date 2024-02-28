fa037cad2ea76b70e672a99444db6e546d5815ce
Ivanilso da Silva Conceicão Filho <66145206+IvanilsoDaSilva@users.noreply.github.com>
2024-02-28 00:59:51 -0300

    Remoção de comentarios do arquivo DatabaseConnection.kt

--- src/main/singleton/database/DatabaseConnection.kt
+++ src/main/singleton/database/DatabaseConnection.kt
@@ -10,12 +10,10 @@ class DatabaseConnection {
 
     public fun getConnection(): Connection {
         if (connection == null) {
-            // Configurações de conexão com o banco de dados
             val url = "jdbc:mysql://localhost:3306/banco_de_dados"
             val username = "usuario"
             val password = "senha"
 
-            // Estabelece a conexão com o banco de dados
             connection = DriverManager.getConnection(url, username, password)
         }
         return connection!!
